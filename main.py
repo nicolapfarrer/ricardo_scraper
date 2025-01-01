e4a9a29f521bf5ddfdbd2596a65da2f5f21635ce
Nicola Pfarrer <nicola.pfarrer@gmail.com>
2025-01-01 23:31:53 +0100

    Refactor main.py to load proxy list URL from environment variables, enhance proxy testing, and implement search configuration loading

--- main.py
+++ main.py
@@ -1,54 +1,76 @@
-import requests,json,random,yaml
+import requests,json,random,os,yaml
+from dotenv import load_dotenv
+#load env variables
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), 'config', '.env'))
+proxy_list_url = os.getenv("PROXY_LIST_URL")
 
-#load config
-def load_config():
 
+def load_search_config():
+    with open(os.path.join(os.path.dirname(__file__), 'config', 'search.yaml')) as file:
+        return yaml.load(file, Loader=yaml.FullLoader)
 
 def is_proxy_working(proxy):
-    test_url = "https://www.google.com"
+    test_url = "https://ricardo.ch"
     proxies = {
         "http": proxy,
         "https": proxy,
     }
     try:
-        response = requests.get(test_url, proxies=proxies, timeout=5)
+        response = requests.get(test_url, proxies=proxies, timeout=10)
         if response.status_code == 200:
             return True
     except:
         return False
     return False
 
-
 def get_random_proxy():
-    gh_list= "https://raw.githubusercontent.com/vakhov/fresh-proxy-list/refs/heads/master/https.txt"
     done=False
     try:
-        response = requests.get(gh_list)
+        response = requests.get(proxy_list_url)
         lines = response.text.splitlines()
         proxys = [line.strip() for line in lines]
     except:
         return None
     while not done:
+            if len(proxys)==0:
+                return None
             proxy= random.choice(proxys)
+            proxys.remove(proxy)
             print(f"Tesing: {proxy}")
             done = is_proxy_working(proxy)
     return proxy
 
 def get_data(criteria):
-    done=False
+    done = False
     while not done:
-        p= get_random_proxy()
+        p = get_random_proxy()
         proxies = {
             "http": p,
             "https": p,
         }
         print(f"Using proxy: {p}")
         try:
-            response = requests.get(f"https://www.ricardo.ch/api/mfa/search/?sort=newest&categorySeoSlug=grafikkarte-39204", proxies=proxies)#temporary url
+            base_url = "https://www.ricardo.ch/api/mfa/search/"
+            query_string = "&".join([f"{key}={value}" for key, value in criteria.items()])
+            url = f"{base_url}?{query_string}"
+            response = requests.get(url, proxies=proxies)
             print(response.status_code)
             data = response.json()
             return data
         except:
             pass
+    return None
 
-print(get_data('grafikkarte-39204'))
+def search_all_configs():
+    search_configs = load_search_config()
+    results = {}
+    for key, config in search_configs.items():
+        print(f"Searching for {key} with config: {config}")
+        data = get_data(config)
+        results[key] = data
+    return results
+
+
+if __name__ == '__main__':
+    all_results = search_all_configs()
+    print(all_results)
